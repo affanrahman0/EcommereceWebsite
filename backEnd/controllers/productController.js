a1785121c1336dade07f4b55b90d4b82dd7b6d97
Affan Rahman <rahmanaffan0@gmail.com>
2024-01-27 19:35:16 +0530

    Commit message describing the changes in frontend2

--- backEnd/controllers/productController.js
+++ backEnd/controllers/productController.js
@@ -1,10 +1,8 @@
-const Product = require("../models/productModels")
+const Product = require("../models/productModels");
 const mongoose = require("mongoose");
 const ErrorHandler = require("../utils/errorHandlers");
 const asyncErrors = require("../middleware/AsyncErrors");
 const ApiFeatures = require("../utils/apiFeatures");
-
-
 
 //create Product
 // The main purpose of the asynchronous function is to interact with a database using the await keyword. The line const product = await Product.create(req.body); is awaiting the completion of the Product.create operation, which involves interacting with a database to create a new product entry.
@@ -13,184 +11,187 @@ const ApiFeatures = require("../utils/apiFeatures");
 
 //create product -- Admin
 exports.createProduct = asyncErrors(async (req, res, next) => {
-    req.body.user = req.user.id //_id can be acessed as id express js does this for us
-    const product = await Product.create(req.body);
-    res.status(201).json({
-        success: true,
-        product
-    });
+  req.body.user = req.user.id; //_id can be acessed as id express js does this for us
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
 });
 
-
 //get all products
-exports.getAllProducts = asyncErrors(async (req, res,next) => {
-    let ProductCount = await Product.countDocuments() //no. of products 
-    let ResultsPerPage = 8 // no .of products to be displayed per page
-    //apllying additional filters on top of search
-    const apiFeatures = new ApiFeatures(Product, req.query).search().filter().pagination(ResultsPerPage) //passing query and querystr
-    const products = await apiFeatures.query;
-    res.status(200).json({
-        success: true,
-        products,
-        ProductCount
-    });
+exports.getAllProducts = asyncErrors(async (req, res, next) => {
+  let ProductCount = await Product.countDocuments(); //no. of products
+  let ResultsPerPage = 8; // no .of products to be displayed per page
+
+   
+  //In js functions uses pass by references
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter();
+
+  let products = await apiFeature.query;
+
+  let filteredProductsCount = products.length;
+
+  apiFeature.pagination(ResultsPerPage);
+
+  products = await apiFeature.query.clone();
+
+  res.status(200).json({
+    success: true,
+    products,
+    ProductCount,
+    ResultsPerPage,
+    filteredProductsCount,
+  });
 });
 
 //Get Product Details
 exports.getProductDetails = asyncErrors(async (req, res, next) => {
-    product = await Product.findById(req.params.id)
+  product = await Product.findById(req.params.id);
 
-    if (product) {
-        res.status(200).json({
-            sucess: true,
-            product
-        })
-    }
-    else {
-        return next(new ErrorHandler("Product not found", 400))
-    }
+  if (product) {
+    res.status(200).json({
+      sucess: true,
+      product,
+    });
+  } else {
+    return next(new ErrorHandler("Product not found", 400));
+  }
 });
 
 //Update products --Admin
 exports.updateProduct = asyncErrors(async (req, res, next) => {
-    // console.log(req.body)
-    let product = await Product.findById(req.params.id)
+  // console.log(req.body)
+  let product = await Product.findById(req.params.id);
 
-    if (!product) {
-        return next(new ErrorHandler("Product not found", 500))
-    }
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 500));
+  }
 
-    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-        useFindAndModify: false
-    })
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
 
-    res.status(200).json({
-        success: true,
-        product
-    });
-
+  res.status(200).json({
+    success: true,
+    product,
+  });
 });
-
 
 //Delete products --Admin
 exports.deleteProduct = asyncErrors(async (req, res, next) => {
-    // Find and delete the product by its ID
-    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+  // Find and delete the product by its ID
+  const deletedProduct = await Product.findByIdAndDelete(req.params.id);
 
-    // If product is not found, return an error response
-    if (!deletedProduct) {
-        return next(new ErrorHandler("Product not found", 400))
-    }
+  // If product is not found, return an error response
+  if (!deletedProduct) {
+    return next(new ErrorHandler("Product not found", 400));
+  }
 
-    // Send a success response indicating the product has been deleted
-    res.status(200).json({
-        success: true,
-        message: "Product deleted successfully"
-    });
+  // Send a success response indicating the product has been deleted
+  res.status(200).json({
+    success: true,
+    message: "Product deleted successfully",
+  });
 });
-
-
 
 exports.createProductReview = asyncErrors(async (req, res, next) => {
-    const { rating, commentin, productId } = req.body; //this variable should exactlt match with the body that u are sending
+  const { rating, commentin, productId } = req.body; //this variable should exactlt match with the body that u are sending
 
-    const review = {
-        name: req.user.name,
-        user: req.user._id,
-        rating: Number(rating),
-        Comment: commentin
-    };
+  const review = {
+    name: req.user.name,
+    user: req.user._id,
+    rating: Number(rating),
+    Comment: commentin,
+  };
 
+  const product = await Product.findById(productId);
 
-    const product = await Product.findById(productId);
+  //if the user has provided a riview previously
+  const isReviewed = product.reviews.find(
+    (rev) => rev.user.toString() === req.user.id.toString()
+  );
 
-    //if the user has provided a riview previously
-    const isReviewed = product.reviews.find(
-        (rev) => rev.user.toString() === req.user.id.toString()
-    );
-
-    //
-    if (isReviewed) {
-
-        product.reviews.forEach((rev) => {
-            if (rev.user.toString() === req.user.id.toString()) {
-                rev.Comment = commentin; // updating the previous review
-                rev.rating = rating;
-            }
-        });
-    } else {
-        product.reviews.push(review); // adding a new review
-        product.numOfreviews = product.reviews.length;
-    }
-
-    let sum = 0;
-
+  //
+  if (isReviewed) {
     product.reviews.forEach((rev) => {
-        sum += rev.rating;
+      if (rev.user.toString() === req.user.id.toString()) {
+        rev.Comment = commentin; // updating the previous review
+        rev.rating = rating;
+      }
     });
+  } else {
+    product.reviews.push(review); // adding a new review
+    product.numOfreviews = product.reviews.length;
+  }
 
-    product.ratings = sum / product.reviews.length; // avg rating of the product
+  let sum = 0;
 
-    await product.save({ validateBeforeSave: false });// you're allowing the data to be saved without being checked against the schema's validation rules that has been defined by you
+  product.reviews.forEach((rev) => {
+    sum += rev.rating;
+  });
 
-    res.status(200).json({
-        success: true,
-        message: "Review added successfully",
-        product
-    });
+  product.ratings = sum / product.reviews.length; // avg rating of the product
+
+  await product.save({ validateBeforeSave: false }); // you're allowing the data to be saved without being checked against the schema's validation rules that has been defined by you
+
+  res.status(200).json({
+    success: true,
+    message: "Review added successfully",
+    product,
+  });
 });
 
-exports.getAllReviews = asyncErrors(async (req,res,next) =>{
+exports.getAllReviews = asyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
 
+  if (!product) return next(new ErrorHandler("Product not found", 400));
 
-    const product = await Product.findById(req.query.id)
-
-    if(!product) return next(new ErrorHandler("Product not found",400))
-
-    res.status(200).json({
-        success: true,
-        reviews : product.reviews
-    })
-})
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews,
+  });
+});
 
 //delete a review by id
-exports.deleteReview = asyncErrors(async (req,res,next) =>{
+exports.deleteReview = asyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.query.productId);
 
+  if (!product) return next(new ErrorHandler("Product not found", 400));
 
-    const product = await Product.findById(req.query.productId)
+  const reviews = product.reviews.filter(
+    (rev) => rev._id.toString() != req.query.id.toString()
+  );
 
-    if(!product) return next(new ErrorHandler("Product not found",400))
+  let sum = 0;
 
-    const reviews = product.reviews.filter(
-        (rev) => rev._id.toString() != req.query.id.toString()
-    )
+  reviews.forEach((rev) => {
+    sum += rev.rating;
+  });
 
-    let sum = 0;
+  let ratings = reviews.length == 0 ? 0 : sum / reviews.length;
+  const numOfreviews = reviews.length;
 
-    reviews.forEach((rev) => {
-        sum += rev.rating;
-    });
+  await Product.findByIdAndUpdate(
+    req.query.productId,
+    {
+      reviews,
+      ratings,
+      numOfreviews,
+    },
+    {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    }
+  );
 
-    let ratings = (reviews.length==0)? 0 : sum / reviews.length;
-    const numOfreviews = reviews.length;
-
-
-    await Product.findByIdAndUpdate(req.query.productId,{
-        reviews,
-        ratings,
-        numOfreviews
-        
-    },{
-        new : true,
-        runValidators : true,
-        useFindAndModify : false
-    })
-
-
-    res.status(200).json({
-        success: true,
-        message : "review deleted"
-    })
-})
+  res.status(200).json({
+    success: true,
+    message: "review deleted",
+  });
+});
--- backEnd/utils/apiFeatures.js
+++ backEnd/utils/apiFeatures.js
@@ -1,5 +1,5 @@
 // http://localhost:4000/api/v1/products?keywords=guava // anything after ? is the querystr and query is an instance of product defined by the monggose schema 
-
+//If the value associated with key is a primitive type (e.g., a string, number, boolean), modifying the value in the shallow copy will not affect the original object. This is because primitive values are copied by value, and changes in the copy do not impact the original. but if the value of key is another nested object,it's value will be changed on modifying the copied nested object 
 class ApiFeatures {
     constructor(query, querystr) {
         this.query = query
@@ -28,7 +28,8 @@ class ApiFeatures {
         const queryCopy = {...this.querystr} // using shallowcopy otherwise it will be a refrence
 
         const removeFields = ["keyword","page","limit"];
-
+        
+        //we are removing the keys from queryCopy only since its a shallow copy, this.querstr remains same
         removeFields.forEach((key) => delete queryCopy[key])
 
 
@@ -43,7 +44,7 @@ class ApiFeatures {
 
         this.query = this.query.find(JSON.parse(queryString)) //this case sensitive search
         
-        console.log(queryString)
+       // console.log(queryString)
 
         return this;
     }
